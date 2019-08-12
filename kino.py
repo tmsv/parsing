40604530c2ffa37e75013aab24875492ad1e5f96
tmsv <42784577+tmsv@users.noreply.github.com>
2019-08-12 17:40:51 +0300

    Trying to use useragents and proxies to get data

--- kino.py
+++ kino.py
@@ -1,11 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
+import csv
+from datetime import datetime
+from random import choice
 
 
-
-
-def get_html(url):
-	r = requests.get(url)
+def get_html(url, useragent=None, proxy=None):
+	r = requests.get(url, headers=useragent, proxies=proxy)
 	return r.text
 
 
@@ -18,27 +19,46 @@ def get_all_links(html):
 	for tag in tags:
 		tail = tag.get('href')
 		links.append('https://www.kinopoisk.ru' + tail)
-	
+	for i in links[:10]:
+		print(i)
 	return links
 
+def get_page_data(html):
+	soup = BeautifulSoup(html, 'lxml')
+	try:
+		title = soup.find('h1', class_='moviename-big').text
+	except:
+		title = ''
+	data = {'title': title}
+	return data
+
+
+def write_csv(data):
+	with open('kinopoisk.csv', 'a') as file:
+		writer = csv.writer(file)
+
+		writer.writerow( (data['title']) )
+		print(data['title'] + ' parsed')
 
 
 def main():
+	start = datetime.now()
+	useragents = open('useragents.txt').read().split('\n')
+	proxies = open('proxies.txt').read().split('\n')
+
 	url = 'https://www.kinopoisk.ru/top/'
 
-	all_links = get_all_links(get_html(url))
+	useragent = {'User_Agent': choice(useragents)}
+	proxy = {'http': 'http://' + choice(proxies)}	
+
+	all_links = get_all_links(get_html(url, useragent, proxy))
+
+	print(all_links)
 	
-	#я подаю вот эту вот алл линкс в функцию которая берет каждый элемент списка и кидает как линку в гет_хтмл.
-	#затем получает синтаксическую версию каждой из 250 страниц по очереди и по очереди же парсит, 
-	#доставая оттуда название фильма, оценку на кинопоиске, оценку на аймдиби и ченить еще
-
-
-
-
-
-
-
-
+	for i in all_links[:2]:
+		useragent = {'User_Agent': choice(useragents)}
+		proxy = {'http': 'http://' + choice(proxies)}
+		write_csv(get_page_data(get_html(i, useragent, proxy)))
 
 if __name__ == '__main__':
 	main()
